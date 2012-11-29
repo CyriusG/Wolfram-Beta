0bb106220179a6154feca494d262bebac6cb5e42
CyriusG <cyriusg94@gmail.com>
2012-11-29 17:05:43 +0100

    Fortsatte arbetet med att skriva bild generatorn. Den kan nu känna igen olika block i en ekvation och behandla dem separat för att enkelt kunna omvandla texten till något som man kan göra en bild utav.
    Fortsätta nu med att skriva klart en tidig fungerande version och börja
    med att kommentera den så att de andra kan använda sig utav den för att
    generera bilder.

--- wolfram-beta/src/se/portalen/wolframbeta/TestingLatex.java
+++ wolfram-beta/src/se/portalen/wolframbeta/TestingLatex.java
@@ -5,12 +5,17 @@ import org.scilab.forge.jlatexmath.TeXConstants;
 import org.scilab.forge.jlatexmath.TeXFormula;
 import org.scilab.forge.jlatexmath.TeXIcon;
 
+import sun.font.LayoutPathImpl.EndType;
+
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Insets;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 import javax.swing.JLabel;
@@ -18,53 +23,19 @@ import javax.swing.JLabel;
 
 public class TestingLatex {
 	public static void main(String[] args) {
-		String doMath = parseText("5 + 3 / 5");
-		generateImage(doMath);
-	}
-	
-	public static String parseText(String input) {
 		
-		input.replace(" ", "");
+		String input = "(5 + 3) / (7x^5 + 56x^2)"; 
 		
-		String result = "";
+		System.out.println(constructMath(input));
 		
-		for (int i = 0; i < input.length(); i++) {
-			result = result + identiySign(String.valueOf(input.charAt(i))) + " ";
-		}
+		/*String math = "y = \\{5 + 3}{7x^5} + 56x^2";
 		
-		return result;
-	}
-	
-	public static String identiySign(String sign) {
-		String[] signs = new String[5];
-		String result = "";
+		System.out.println(math);
 		
-		System.out.println(sign);
+		math = math.replaceAll("\\\\", "\\\\frac");
 		
-		signs[0] = "+";
-		signs[1] = "-";
-		signs[2] = "/";
-		signs[3] = "\\";
-		signs[4] = "*";
-				
-		for (int i = 0; i < signs.length; i++) {
-			if(sign.equals(signs[i])) {
-				result = signs[i];
-				break;
-			}
-		}
+		System.out.println(math);
 		
-		if(signs.equals("")) {
-			result = sign;;
-		}
-		else if(sign.equals(signs[2]) || signs.equals(signs[3])) {
-			result = "\\over";
-		}
-		
-		return result;
-	}
-	
-	public static void generateImage(String math) {
 		TeXFormula fomule = new TeXFormula(math);
 	    TeXFormula formula = new TeXFormula(fomule);
 	    TeXIcon icon = formula.createTeXIcon(TeXConstants.STYLE_DISPLAY, 20);
@@ -83,6 +54,91 @@ public class TestingLatex {
 	        ImageIO.write(image, "png", file.getAbsoluteFile());
 	    } catch (IOException ex) {
 	        ex.getMessage();
-	    }
+	    }*/
+	}
+	
+	public static String constructMath(String input) {
+		ArrayList<String> mathConstruction = new ArrayList<String>();
+		ArrayList<String> constructedBlocks = new ArrayList<String>();
+		String constructBlock = "";
+		int lastBlock = -1;
+		String result = "";
+		String mergedBlock = "";
+		
+		input = input.replace(" ", "");
+		
+		for (int i = 0; i < input.length(); i++) {
+			mathConstruction.add(String.valueOf(input.charAt(i)));
+		}
+		
+		int startIndex = 0;
+		int endIndex = 0;
+		for (int i = 0; i < mathConstruction.size(); i++) {
+			if(mathConstruction.get(i).equals("/") || mathConstruction.get(i).equals("*") || mathConstruction.get(i).equals("+") || mathConstruction.get(i).equals("-")) {
+				for (int j = i; j > 0; j--) {
+					if(mathConstruction.get(j).equals("/") || mathConstruction.get(j).equals("*") || mathConstruction.get(j).equals("+") || mathConstruction.get(j).equals("-")) {
+						startIndex = lastBlock + 1;
+						endIndex = j;
+						lastBlock = i;
+						break;
+					}
+				}
+				constructBlock = "";
+				for (int j = startIndex; j < endIndex; j++) {
+					constructBlock = constructBlock + mathConstruction.get(j);
+					if(j == endIndex - 1) {
+						constructedBlocks.add(constructBlock);
+						constructedBlocks.add(mathConstruction.get(endIndex));
+					}
+				}
+				
+				
+			}
+			if(i == mathConstruction.size() - 1) {
+				constructBlock = "";
+				startIndex = lastBlock + 1;
+				endIndex = mathConstruction.size();
+				
+				for (int j = startIndex; j < endIndex; j++) {
+					constructBlock = constructBlock + mathConstruction.get(j);
+				}
+				
+				constructedBlocks.add(constructBlock);
+			}
+		}
+		
+		for (int i = 0; i < constructedBlocks.size(); i++) {
+			if(i != constructedBlocks.size() - 1) {
+				for (int j = 0; j < constructedBlocks.size(); j++) {
+					if(constructedBlocks.get(i).startsWith("(") && constructedBlocks.get(j).endsWith(")")) {
+						constructedBlocks.set(i, constructedBlocks.get(i).replace("(", ""));
+						constructedBlocks.set(j, constructedBlocks.get(j).replace(")", ""));
+						
+						mergedBlock = constructedBlocks.get(i);
+						for (int j2 = i + 1; j2 < j + 1; j2++) {
+							System.out.println(constructedBlocks.get(j2));
+							System.out.println(j2);
+							
+							mergedBlock = mergedBlock + constructedBlocks.get(j2);
+						}
+						constructedBlocks.set(i, mergedBlock); 
+						for (int j2 = i + 1; j2 < j + 1; j2++) {
+							constructedBlocks.remove(i + 1);
+						}
+					}
+				}
+			}
+		}
+		
+		for (int i = 0; i < constructedBlocks.size(); i++) {
+			
+			
+			
+			result = result + constructedBlocks.get(i);
+		}
+		
+		System.out.println(constructedBlocks);
+		
+		return result;
 	}
 }
