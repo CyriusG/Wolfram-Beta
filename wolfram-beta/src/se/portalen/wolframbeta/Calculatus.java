be780b390aa311754404a15b0446d68c530407e9
Emberg <Emberg94@gmail.com>
2013-02-01 10:07:03 +0100

    Lite småpolering sen senast.

--- wolfram-beta/src/se/portalen/wolframbeta/Calculatus.java
+++ wolfram-beta/src/se/portalen/wolframbeta/Calculatus.java
@@ -1,100 +1,16 @@
 package se.portalen.wolframbeta;
 
-import java.util.ArrayList;
-
 public class Calculatus {
-	static int test101 = 0;
 
-	
 	public static void main(String[]args){
-		System.out.println(trimmer("(((5*((5*(5+3))/7))))"));
-	}
-	
-	public static String mathing(String input){
-		
-		ArrayList<String> mathConstruction = new ArrayList<String>();
-		ArrayList<String> constructedBlocks = new ArrayList<String>();
-		String constructBlock = "";
-		int lastBlock = -1;
-		String result = "";
-		
-		input = input.replace(" ", "");
-		
-		for (int i = 0; i < input.length(); i++) {
-			mathConstruction.add(String.valueOf(input.charAt(i)));
-		}
-		
-		int startIndex = 0;
-		int endIndex = 0;
-		for (int i = 0; i < mathConstruction.size(); i++) {
-			if(mathConstruction.get(i).equals("/") || mathConstruction.get(i).equals("*") || mathConstruction.get(i).equals("+") || mathConstruction.get(i).equals("-")) {
-				for (int j = i; j > 0; j--) {
-					if(mathConstruction.get(j).equals("/") || mathConstruction.get(j).equals("*") || mathConstruction.get(j).equals("+") || mathConstruction.get(j).equals("-")) {
-						startIndex = lastBlock + 1;
-						endIndex = j;
-						lastBlock = i;
-						break;
-					}
-				}
-				constructBlock = "";
-				for (int j = startIndex; j < endIndex; j++) {
-					constructBlock = constructBlock + mathConstruction.get(j);
-					if(j == endIndex - 1) {
-						constructedBlocks.add(constructBlock);
-						constructedBlocks.add(mathConstruction.get(endIndex));
-					}
-				}
-				
-				
-			}
-			if(i == mathConstruction.size() - 1) {
-				constructBlock = "";
-				startIndex = lastBlock + 1;
-				endIndex = mathConstruction.size();
-				
-				for (int j = startIndex; j < endIndex; j++) {
-					constructBlock = constructBlock + mathConstruction.get(j);
-				}
-				
-				constructedBlocks.add(constructBlock);
-			}
-		}
-		
-		for (int i = 0; i < constructedBlocks.size(); i++) {	
-			result = result + "\n" + constructedBlocks.get(i);
-		}
-			
-		return result;
-	}
-	
-	public static String prioritizer(String input) {
-		
-		input = input.replace(" ", "");
-		
-		for (int i = 0; i < input.length(); i++) {
-			System.out.println(i);
-			if (String.valueOf(input.charAt(i)).equals(")")){
-				System.out.println(input.charAt(i));
-				for (int j = i; j >= 0; j--) {
-					System.out.println(j);
-					if (String.valueOf(input.charAt(j)).equals("(")){
-						System.out.println(j);
-						System.out.println(input.charAt(j));
-						String parameter = input.substring(j, i+1);
-						String parameterBuffer = parameter.substring(1, i-1);
-						System.out.println(input);
-						input = input.replace(parameter, (splitter(parameterBuffer) + ""));
-						System.out.println(input);
-					}
-				}
-			} 
-		}
-		input = splitter(input) + "";
-		
-		return input;
+		System.out.println(trimmer("(((  (  2.  5    *pi*e*e  )* 5.  0  ) *2))))))"));
 	}
 	
 	public static String trimmer(String input){
+		
+		input = input.replace(" ", "");
+		input = input.replace("pi", Math.PI+"");
+		input = input.replace("e", Math.E+"");
 		
 		String trim = ""; 
 		
@@ -107,7 +23,7 @@ public class Calculatus {
 						input = input.replace("("+trim+")", splitter(trim)+"");
 						i = 0;
 						break;
-					}
+					} 
 				}
 			} 
 		}
