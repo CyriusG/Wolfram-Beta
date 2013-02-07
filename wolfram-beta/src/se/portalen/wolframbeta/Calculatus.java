e6375b35ac43452bcda83d9320d2bee440a9f479
Emberg <Emberg94@gmail.com>
2013-02-07 12:31:06 +0100

    Nästan färdigställt metoden som tar hand om paranteser i uträkningar. Den är dock inte idiotsäker än.

--- wolfram-beta/src/se/portalen/wolframbeta/Calculatus.java
+++ wolfram-beta/src/se/portalen/wolframbeta/Calculatus.java
@@ -3,16 +3,25 @@ package se.portalen.wolframbeta;
 public class Calculatus {
 
 	public static void main(String[]args){
-		System.out.println(trimmer("(((  (  2.  5    *pi*e*e  )* 5.  0  ) *2))))))"));
+		System.out.println(structurer("((((5*3)/6)*9)+95)-7"));
 	}
 	
-	public static String trimmer(String input){
-		
-		input = input.replace(" ", "");
-		input = input.replace("pi", Math.PI+"");
-		input = input.replace("e", Math.E+"");
+	public static String structurer(String input){
 		
 		String trim = ""; 
+		int leftCount = 0;
+		int rightCount = 0;
+		input = "("+input+")";
+		
+		input = input.replace(" ", "");
+		
+		input = input.replace("pi", Math.PI+"");
+		input = input.replace("Pi", Math.PI+"");
+		input = input.replace("pI", Math.PI+"");
+		input = input.replace("PI", Math.PI+"");
+		
+		input = input.replace("e", Math.E+"");
+		input = input.replace("E", Math.E+"");
 		
 		for (int i = 0; i < input.length(); i++) {	
 			if (String.valueOf(input.charAt(i)).equals(")")){
@@ -25,7 +34,33 @@ public class Calculatus {
 						break;
 					} 
 				}
-			} 
+			}	
+		}
+		
+		for (int i = 0; i < input.length(); i++) {
+			if (String.valueOf(input.charAt(i)).equals(")")){
+				rightCount++;
+			}
+		}
+		
+		for (int i = 0; i < input.length(); i++) {
+			if (String.valueOf(input.charAt(i)).equals("(")){
+				leftCount++;
+			}
+		}
+		
+		int numPar = rightCount-leftCount;
+		
+		if(numPar!=0){
+			if((numPar)>0){
+				for(int i=0; i<numPar; i++){
+					input = input.substring(0, input.length()-1);
+				}
+			} else if((numPar)<0){
+				for(int i=0; i>numPar; i--){
+					input = input.substring(1, input.length());
+				}
+			}
 		}
 	
 		return input;
@@ -39,6 +74,7 @@ public class Calculatus {
 		
 		
 		for (int i = 0; i < parameter.length(); i++) {	
+			
 			if(String.valueOf(parameter.charAt(i)).equals("*")){
 				tehArray = parameter.split("\\*");
 				returnValue = Double.parseDouble(tehArray[0]) * Double.parseDouble(tehArray[1]);
@@ -62,6 +98,11 @@ public class Calculatus {
 				returnValue = Double.parseDouble(tehArray[0]) + Double.parseDouble(tehArray[1]);
 				
 			}
+			
+		}
+		
+		if(returnValue==0.0) {
+			returnValue = Double.parseDouble(parameter);
 		}
 		
 		return returnValue;
