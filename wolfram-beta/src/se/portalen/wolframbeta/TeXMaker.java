a041373f11769312724bf1d1810375b3fbe05c8f
Elev 3C <Elev 3C@Portalen-Dator>
2013-02-07 11:01:00 +0100

    Börjat med att skriva om klassen som genererar latex kod.

--- wolfram-beta/src/se/portalen/wolframbeta/TeXMaker.java
+++ wolfram-beta/src/se/portalen/wolframbeta/TeXMaker.java
@@ -40,18 +40,18 @@ public class TeXMaker {
 			System.out.println("1:" + constructedBlocks);
 			
 			// Detect where brackets are located and parse it
-			detectBracktes();
-			System.out.println("2: " + constructedBlocks);
+			//detectBracktes();
+			//System.out.println("2: " + constructedBlocks);
 			
 			// Format the special signs into something the renderer can read.
-			formatSpecialSigns();
-			System.out.println("3: " + constructedBlocks);
+			//formatSpecialSigns();
+			//System.out.println("3: " + constructedBlocks);
 	
 			for (int i = 0; i < constructedBlocks.size(); i++) {	
 				result = result + constructedBlocks.get(i);
 			}
 			
-			result = result.replace("pi", "$\\pi$");
+			result = result.replace("^pi$", "$\\pi$");
 			result = result.replace("*", "$\\times$");
 			result = result.replaceAll("^e$", "$\\mathrm{e}$");
 			
@@ -67,111 +67,172 @@ public class TeXMaker {
 	 * render later.
 	 */
 	private void blockInput() {
-		// Resets the indexes.
-		int startIndex = 0;
-		int endIndex = 0;
+		
 		
 		for (int i = 0; i < mathConstruction.size(); i++) {
-			if(mathConstruction.get(i).equals("(") && i != 0) {
-				mathConstruction.add(i, "*");
-				break;
-			}
-		}
-		
-		// The main loop that goes through every character in the list.
-		for (int i = 0; i < mathConstruction.size(); i++) {
-			// If it finds a /, *, + or - it have found its first block and makes everything to the left into its own block
-			// and everything to the right that before the next /, *, + or - into another block. It also makes the
-			// sign found it own block.
-			if(mathConstruction.get(i).equals("/") || mathConstruction.get(i).equals("*") || mathConstruction.get(i).equals("+") 
-					|| mathConstruction.get(i).equals("-") || mathConstruction.get(i).equals("%")) 
+			if(
+					(mathConstruction.get(mathConstruction.size() - 1).equals("(") || mathConstruction.get(mathConstruction.size() - 1).equals(")")) &&
+					(mathConstruction.get(0).equals("(") || mathConstruction.get(0).equals(")")) &&
+					(mathConstruction.get(1).equals(")") || mathConstruction.get(1).equals("("))) 
 			{
-				// It starts counting back to find the last sign or if it's the beginning.
-				for (int j = i; j > 0; j--) {
-					// When it reaches the start or the last sign it have found a new block and takes note of the
-					// index.
-					if(mathConstruction.get(j).equals("/") || mathConstruction.get(j).equals("*") || mathConstruction.get(j).equals("+") 
-							|| mathConstruction.get(j).equals("-") || mathConstruction.get(i).equals("%"))
-					{
-						startIndex = lastBlock + 1;
-						endIndex = j;
-						lastBlock = i;
-						break;
-					}
-				}
-				// Makes sure the string that constructs the block is empty.
-				constructBlock = "";
-				// Loops through the list where it found a block and adds all the characters
-				// into a string and then add the string into a list that contains the blocks.
-				for (int j = startIndex; j < endIndex; j++) {
-					
-					constructBlock = constructBlock + mathConstruction.get(j);
-					if(j == endIndex - 1) {
-						constructedBlocks.add(constructBlock);
-						constructedBlocks.add(mathConstruction.get(endIndex));
-					}
-				}
+				mathConstruction.remove(mathConstruction.size() - 1);
 			}
 			
-			// Pretty much same as above. It's just changed for the last block.
-			if(i == mathConstruction.size() - 1) {
-				constructBlock = "";
-				startIndex = lastBlock + 1;
-				endIndex = mathConstruction.size();
-				
-				for (int j = startIndex; j < endIndex; j++) {
-					constructBlock = constructBlock + mathConstruction.get(j);
-				}
-				
-				constructedBlocks.add(constructBlock);
+			if(
+					(mathConstruction.get(0).equals("(") || mathConstruction.get(0).equals(")")) && 
+					((mathConstruction.get(1).equals("(") || mathConstruction.get(1).equals(")")))
+			  ) 
+			{
+				mathConstruction.remove(0);
 			}
 		}
-	}
-	
-	/**
-	 * Detects where brackets are and then puts everything within them into its own block.
-	 */
-	private void detectBracktes() {
-		// Outer loops that goes through the whole list.
-		for (int i = 0; i < constructedBlocks.size(); i++) {
-			// Makes sure it doesn't perform this on the last block which would 
-			// create an error.
-			if(i != constructedBlocks.size() - 1) {
-				// The inner loop that also goes through the whole loop but searches also for brackets.
-				for (int j = 0; j < constructedBlocks.size(); j++) {
-					// When the first loop have found a left bracket and the other loop found a right bracket...
-					if(constructedBlocks.get(i).startsWith("(") && constructedBlocks.get(j).endsWith(")")) {
-						// Remove the brackets and...
-						constructedBlocks.set(i, constructedBlocks.get(i).replace("(", ""));
-						constructedBlocks.set(j, constructedBlocks.get(j).replace(")", ""));
+		
+		String newBlock;
+		boolean startBlocking;
+		int extraBrackets;
+		
+		
+		for (int i = 0; i < mathConstruction.size(); i++) {
+			newBlock = "";
+			startBlocking = false;
+			extraBrackets = 0;
+			
+			if(mathConstruction.get(i).equals("(")) {
+			
+				for (int j = i; j < mathConstruction.size(); j++) {
+					if(startBlocking) {
+						newBlock += mathConstruction.get(j);
 						
-						// Merge all the blocks that's found within the brackets and...
-						mergedBlock = constructedBlocks.get(i);
-						for (int j2 = i + 1; j2 < j + 1; j2++) {						
-							mergedBlock = mergedBlock + constructedBlocks.get(j2);
-						}
+						if(mathConstruction.get(j).equals("("))
+							extraBrackets++;
 						
-						// Remove the not used blocks.
-						constructedBlocks.set(i, mergedBlock); 
-						for (int j2 = i + 1; j2 < j + 1; j2++) {
-							constructedBlocks.remove(i + 1);
-						}
+						if(mathConstruction.get(j).equals(")") && extraBrackets > 0) {
+							extraBrackets--;
+							newBlock += mathConstruction.get(j + 1);
+							if(extraBrackets == 0) {
+								i = j + 1;
+								break;
+							}
+						}					
+					}
+					
+					if(mathConstruction.get(i).equals("(")) {
+						newBlock += mathConstruction.get(i);
+						startBlocking = true;
 					}
 				}
+				System.out.println(newBlock);
 			}
+			constructedBlocks.add(newBlock);
 		}
+		
+		
+		
+//		// Resets the indexes.
+//		int startIndex = 0;
+//		int endIndex = 0;
+//		
+//		for (int i = 0; i < mathConstruction.size(); i++) {
+//			if(mathConstruction.get(i).equals("(") && i != 0) {
+//				mathConstruction.add(i, "*");
+//				break;
+//			}
+//		}
+//		
+//		// The main loop that goes through every character in the list.
+//		for (int i = 0; i < mathConstruction.size(); i++) {
+//			// If it finds a /, *, + or - it have found its first block and makes everything to the left into its own block
+//			// and everything to the right that before the next /, *, + or - into another block. It also makes the
+//			// sign found it own block.
+//			if(mathConstruction.get(i).equals("/") || mathConstruction.get(i).equals("*") || mathConstruction.get(i).equals("+") 
+//					|| mathConstruction.get(i).equals("-") || mathConstruction.get(i).equals("%")) 
+//			{
+//				// It starts counting back to find the last sign or if it's the beginning.
+//				for (int j = i; j > 0; j--) {
+//					// When it reaches the start or the last sign it have found a new block and takes note of the
+//					// index.
+//					if(mathConstruction.get(j).equals("/") || mathConstruction.get(j).equals("*") || mathConstruction.get(j).equals("+") 
+//							|| mathConstruction.get(j).equals("-") || mathConstruction.get(i).equals("%"))
+//					{
+//						startIndex = lastBlock + 1;
+//						endIndex = j;
+//						lastBlock = i;
+//						break;
+//					}
+//				}
+//				// Makes sure the string that constructs the block is empty.
+//				constructBlock = "";
+//				// Loops through the list where it found a block and adds all the characters
+//				// into a string and then add the string into a list that contains the blocks.
+//				for (int j = startIndex; j < endIndex; j++) {
+//					
+//					constructBlock = constructBlock + mathConstruction.get(j);
+//					if(j == endIndex - 1) {
+//						constructedBlocks.add(constructBlock);
+//						constructedBlocks.add(mathConstruction.get(endIndex));
+//					}
+//				}
+//			}
+//			
+//			// Pretty much same as above. It's just changed for the last block.
+//			if(i == mathConstruction.size() - 1) {
+//				constructBlock = "";
+//				startIndex = lastBlock + 1;
+//				endIndex = mathConstruction.size();
+//				
+//				for (int j = startIndex; j < endIndex; j++) {
+//					constructBlock = constructBlock + mathConstruction.get(j);
+//				}
+//				
+//				constructedBlocks.add(constructBlock);
+//			}
+//		}
 	}
 	
-	public void formatSpecialSigns() {
-		for (int i = 0; i < constructedBlocks.size(); i++) {
-			if(constructedBlocks.get(i).equals("/") && i > 0) {
-				String firstBlock = constructedBlocks.get(i - 1);
-				String secondBlock = constructedBlocks.get(i + 1);
-				
-				constructedBlocks.set(i - 1, "$\\frac {" + firstBlock + "}{" + secondBlock + "}$");
-				constructedBlocks.remove(i);
-				constructedBlocks.remove(i);
-			}
-		}
-	}
+//	/**
+//	 * Detects where brackets are and then puts everything within them into its own block.
+//	 */
+//	private void detectBracktes() {
+//		// Outer loops that goes through the whole list.
+//		for (int i = 0; i < constructedBlocks.size(); i++) {
+//			// Makes sure it doesn't perform this on the last block which would 
+//			// create an error.
+//			if(i != constructedBlocks.size() - 1) {
+//				// The inner loop that also goes through the whole loop but searches also for brackets.
+//				for (int j = 0; j < constructedBlocks.size(); j++) {
+//					// When the first loop have found a left bracket and the other loop found a right bracket...
+//					if(constructedBlocks.get(i).startsWith("(") && constructedBlocks.get(j).endsWith(")")) {
+//						// Remove the brackets and...
+//						constructedBlocks.set(i, constructedBlocks.get(i).replace("(", ""));
+//						constructedBlocks.set(j, constructedBlocks.get(j).replace(")", ""));
+//						
+//						// Merge all the blocks that's found within the brackets and...
+//						mergedBlock = constructedBlocks.get(i);
+//						for (int j2 = i + 1; j2 < j + 1; j2++) {						
+//							mergedBlock = mergedBlock + constructedBlocks.get(j2);
+//						}
+//						
+//						// Remove the not used blocks.
+//						constructedBlocks.set(i, mergedBlock); 
+//						for (int j2 = i + 1; j2 < j + 1; j2++) {
+//							constructedBlocks.remove(i + 1);
+//						}
+//					}
+//				}
+//			}
+//		}
+//	}
+//	
+//	public void formatSpecialSigns() {
+//		for (int i = 0; i < constructedBlocks.size(); i++) {
+//			if(constructedBlocks.get(i).equals("/") && i > 0) {
+//				String firstBlock = constructedBlocks.get(i - 1);
+//				String secondBlock = constructedBlocks.get(i + 1);
+//				
+//				constructedBlocks.set(i - 1, "$\\frac {" + firstBlock + "}{" + secondBlock + "}$");
+//				constructedBlocks.remove(i);
+//				constructedBlocks.remove(i);
+//			}
+//		}
+//	}
 }
--- wolfram-beta/src/se/portalen/wolframbeta/TestingLatex.java
+++ wolfram-beta/src/se/portalen/wolframbeta/TestingLatex.java
@@ -2,11 +2,10 @@ package se.portalen.wolframbeta;
 
 public class TestingLatex {
 	public static void main(String[] args) {
-		EquationGen equationGen = new EquationGen();
 		TeXMaker texPareser = new TeXMaker();
 		
 		
 		
-		System.out.println("Hai");
+		System.out.println(texPareser.parseTex("(2 * (2/3)) + 5x"));
 	}
 }
