6a555742dfca8195737937c4582dae4ec84962d4
Emberg <Emberg94@gmail.com>
2013-03-03 04:55:23 +0100

    Ett tag sedan jag uppdaterade senast. Rensade upp koden(tog bort överflödig testkod, fixade ordentliga variabelnamn och dylikt) och lade till 2 nya metoder: En som tar bort överflödiga paranteser, och en metod som agerar tillägg till den första (i och med hur den första är skriven, måste denna metoden lägga till paranteser på någon sida för att uträkningsmetoden skall kunna hantera strängen den får).

--- wolfram-beta/src/se/portalen/wolframbeta/Calculatus.java
+++ wolfram-beta/src/se/portalen/wolframbeta/Calculatus.java
@@ -3,64 +3,106 @@ package se.portalen.wolframbeta;
 public class Calculatus {
 
 	public static void main(String[]args){
-		System.out.println(structurer("((((5*3)/6)*9)+95)-7"));
+		System.out.println(parStructurer("((())))))((())((pi)))(((()*3)))))+3)+5)"));
 	}
 	
-	public static String structurer(String input){
-		
-		String trim = ""; 
+	public static String parSupplement(String input) {
+		char[] cArray = input.toCharArray();
+		int length = cArray.length;
 		int leftCount = 0;
 		int rightCount = 0;
-		input = "("+input+")";
+		String returnValue = "";
 		
+		for(int i = 0; i < length; i++){
+			if(String.valueOf(input.charAt(i)).equals("(")){
+				leftCount++;
+			} else if(String.valueOf(input.charAt(i)).equals(")")){
+				rightCount++;
+			}
+		}
+		
+		int difference = leftCount-rightCount;
+		
+		if(difference>0){
+			char[] tempArray = new char[length+difference];
+			
+			for(int i = 0; i < length; i++){
+				tempArray[i] = cArray[i];
+			}
+			for(int j = 0; j < difference; j++){
+				tempArray[length+j] = ')';
+			}
+			for(char a : tempArray){
+				returnValue = returnValue + a;
+			}
+			
+		} else if(difference<0){
+			difference = difference*(-1);
+			char[] tempArray = new char[length+difference];
+			
+			for(int i = 0; i < difference; i++){
+				tempArray[i] = '(';
+			}
+			for(int j = 0; j < length; j++){
+				tempArray[difference+j] = cArray[j];
+			}
+			for(char a : tempArray){
+				returnValue = returnValue + a;
+			}
+		}
+
+		return returnValue;
+	}
+	
+	public static String parCleaner(String input) {
+		char[] cArray = input.toCharArray();
+		int counter = 0;
+		String returnVal = "";
+		
+		for(int i = 0; i < cArray.length; i++){
+			if(((String.valueOf(input.charAt(i)).equals("(")) || (String.valueOf(input.charAt(i)).equals(")"))) && (counter==0)){
+				counter++;
+			} else if ((String.valueOf(input.charAt(i)).equals("(")) && (counter!=0)){
+				cArray[i] = ' ';
+			} else if ((String.valueOf(input.charAt(i)).equals(")")) && (counter!=0)){
+				cArray[i] = ' ';
+			} else {
+				counter = 0;
+			}
+		}
+		
+		for(char a : cArray) {
+		    returnVal = returnVal + a;
+		}
+		
+		return returnVal.replace(" ", "");
+	}
+	
+	public static String parStructurer(String input){
 		input = input.replace(" ", "");
-		
 		input = input.replace("pi", Math.PI+"");
 		input = input.replace("Pi", Math.PI+"");
 		input = input.replace("pI", Math.PI+"");
 		input = input.replace("PI", Math.PI+"");
-		
 		input = input.replace("e", Math.E+"");
 		input = input.replace("E", Math.E+"");
+		
+		input = parCleaner(input);
+		input = parSupplement(input);
+		
+		String trim = ""; 
 		
 		for (int i = 0; i < input.length(); i++) {	
 			if (String.valueOf(input.charAt(i)).equals(")")){
 				for (int j = i; j >= 0; j--) {
 					if (String.valueOf(input.charAt(j)).equals("(")){
 						trim = input.substring(j+1, i);
-						System.out.println(input);
 						input = input.replace("("+trim+")", splitter(trim)+"");
 						i = 0;
 						break;
 					} 
 				}
 			}	
-		}
-		
-		for (int i = 0; i < input.length(); i++) {
-			if (String.valueOf(input.charAt(i)).equals(")")){
-				rightCount++;
-			}
-		}
-		
-		for (int i = 0; i < input.length(); i++) {
-			if (String.valueOf(input.charAt(i)).equals("(")){
-				leftCount++;
-			}
-		}
-		
-		int numPar = rightCount-leftCount;
-		
-		if(numPar!=0){
-			if((numPar)>0){
-				for(int i=0; i<numPar; i++){
-					input = input.substring(0, input.length()-1);
-				}
-			} else if((numPar)<0){
-				for(int i=0; i>numPar; i--){
-					input = input.substring(1, input.length());
-				}
-			}
 		}
 	
 		return input;
